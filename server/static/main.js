f29180a4fa4dc1d64cc75428c7441c561f33e88f
jod35 <jodestrevin@gmail.com>
2023-05-24 14:34:03 +0300

    add paraphrase button

--- server/static/main.js
+++ server/static/main.js
@@ -2,8 +2,11 @@ const form = document.querySelector('#summary-form');
 const sourceTextInput = document.getElementById('sourceTextInput');
 const resultTextInput = document.querySelector("#summarizedText");
 const resetButton = document.querySelector("#resetButton");
+const paraphraseButton = document.querySelector("#paraphrase-btn");
+const copyButton = document.querySelector("#copy-btn");
 const sourceLengthText = document.getElementById("source_text_len");
 const resultTextLength = document.getElementById("result_text_len");
+const loadingGif = document.querySelector('.loading');
 
 let sourceLength = 0;
 let resultLength = 0;
@@ -20,12 +23,15 @@ const summarizeText = async (text)=>{
         },
         body:JSON.stringify({text})
     }
+
     const response = await fetch('/',requestOptions);
+    loadingGif.style.visibility = 'visible';
 
     const responseText = await response.json();
 
-    console.log(responseText)
     resultTextInput.textContent = responseText.summarizedText;
+    loadingGif.style.visibility = 'hidden';
+
     resultTextLength.innerText = `${responseText.length} characters`;
 
 }
@@ -57,14 +63,28 @@ const resetFormAndOutput = ()=>{
     sourceLengthText.innerText = `${0} characters`;
 }
 
+// copy text
+const copyText = async ()=>{
+    try{
+        await navigator.clipboard.writeText(resultTextInput.value);
+
+        alert("Text copied")
+    }
+    catch(error){
+        console.log(error)
+    }
+}
 
 
 
-//handle events
+// send text to server
 form.addEventListener('submit',submitFormtoServer);
 
+// reset values
 resetButton.addEventListener('click',resetFormAndOutput);
 
+// handle source text summarization
 sourceTextInput.addEventListener('input',handleSourceTextInput);
 
-console.log(form)
+// copy text
+copyButton.addEventListener('click',copyText);
